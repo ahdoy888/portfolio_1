3c5a3d75a9a02b965bf1ebd5e6a8435d3cfa6fdc
hungfaikoo <jhk88@hotmail.com>
2019-09-18 15:56:30 -0700

    minor changes

--- src/components/Resume/Main/Main.js
+++ src/components/Resume/Main/Main.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import Picture1 from './resume_01.png';
-import Picture2 from './resume_02.png';
+import Picture1 from './resume_001.png';
+
 
 import './Main.css';
-
 const Main = () => {
-        return (
-          <>
-					
-					<div id='resume' className="resume2">
-						<div>
-						<img className='page-one' src={Picture1} alt="resume page one" />
-						<img className='page-one' src={Picture2} alt="resume page one" />
-						</div>
-						</div>
-						
-					</>  
-        )
+       return (
+         <>
+          <div id='resume' className="resume2">
+            <div>
+              <img className='page-one' src={Picture1} alt="resume page one" />
+            </div>             
+          </div>
+                        
+        </>
+       )
 }
-
 export default Main;
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './Header/Header';
-import Comment from './Comment/Comment';
 import Footer from './Footer/Footer';
 import Main from './Main/Main';
 
