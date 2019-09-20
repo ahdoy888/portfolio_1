d410e8f77523eee9368f1c455932c70d56653c8a
hungfaikoo <jhk88@hotmail.com>
2019-09-19 20:18:50 -0700

    changes

--- src/components/Landing/Header/Header.js
+++ src/components/Landing/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
       <header className="vertical-center header-landing">
         <div>
-          <h1><a href="/">Hi, My name is <strong className="name">Jacky Koo</strong>.<br />I'm Fullstack Developer</a></h1>
+          <h1><a href="/">Hi, My name is <strong className="name">Jacky Koo</strong>.<br />I'm a Fullstack Developer</a></h1>
         </div>
       </header>
     )
