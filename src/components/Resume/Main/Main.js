b1ae51851dcc7c8c1bf79e8cd8062da0c6f47a46
hungfaikoo <jhk88@hotmail.com>
2019-11-04 19:44:36 -0800

    made minor changes resume

--- src/components/Resume/Main/Main.js
+++ src/components/Resume/Main/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Picture1 from './resume_003.png';
-// import Picture2 from './resume_002.png';
+import Picture1 from './resume_004.png';
 
 
 import './Main.css';
@@ -10,7 +9,6 @@ const Main = () => {
           <div id='resume' className="resume2">
             <div>
               <img className='page-one' src={Picture1} alt="resume page one" />
-              {/* <img className='page-one' src={Picture2} alt="resume page one" /> */}
             </div>             
           </div>
                         
